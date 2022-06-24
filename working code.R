bb4ebc847902dffdb890774610247eb93160702c
Claudia Donahue <claudialdonahue@gmail.com>
2022-06-24 10:42:59 -0400

    got a function working finally

--- working code.R
+++ working code.R
@@ -1,42 +1,44 @@
-base <- "https://pokeapi.co/api/v2/pokemon/"
 
-getAll <- function(url){
-  out <- NULL
-  while (!is.null(url)){
+countryCases <- function(country="all"){
+  ###
+  # This function returns a data frame with data on Covid cases. It can also
+  # return those columns for a single country if a country's name or abbreviation is passed.
+  ###
+  
+  # Get the country data from the summaryRoute endpoint.
+  outputAPI <- fromJSON(
+    "https://api.covid19api.com/summary"
+  )
+  
+  # Select only the data.frame from the JSON output.
+  output <- outputAPI$Countries
+  
+  # If country does not equal "all", check if it is an abbrev or country name.
+  if (country != "all"){
     
-    request <- GET(url)
-    contentPokemon <- content(request)
-    out <- c(out, contentPokemon$results)
-    url <- contentPokemon$`next`
+    # If country is in the CountryCode column, subset output for just that row.
+    if (country %in% output$CountryCode){
+      output <- output %>%
+        filter(CountryCode == country)
+    }
+    # If country is in the Country column, subset output for just that row.
+    else if (country %in% output$Country){
+      output <- output %>%
+        filter(Country == country)
+    }
+    # Otherwise, throw an informative error.
+    else {
+      message <- paste("ERROR: Argument for country was not found in either",
+                       "the Country or CountryCode columns. Try ('all') to",
+                       "find the country you're looking for.")
+      stop(message)
+    }
   }
-  out
-}
-pokemon <- getAll("base")
-str(pokemon[[1]])
-
-lookup <- function(url){
-  all <- getAll(url)
+  # Do nothing if the country value equals "all".
+  else {
+    
+  }
   
-  url <- all %>% map_chr("url")
-  name <- all %>% map_chr(name)
-  #name[name == "unknown"] <- NA
-  
-  set_names(name)
+ # Return the output data frame.
+  return(output)
 }
-
-nameList <- lookup(base)
-
-df <- data.frame()
-
-for (name in nameList){
-  call1 <- paste(base, name, sep = "")
-  pokemon <- GET(call1) %>% content()
-  #name <- pokemon$name
-  df$name <- name
-}
-  
-base <- "https://pokeapi.co/api/v2/pokemon/"
-name <- "pikachu"
-call1 <- paste(base, name, sep = "")
-pokemon <- GET(call1) %>% content()
-pokemon$name
